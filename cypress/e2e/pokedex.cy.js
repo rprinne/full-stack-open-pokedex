7e9dd231229ade32e1222fab80f29be47ba4cb49
rprinne <rp.rinne@hotmail.com>
2025-12-31 22:20:16 +0200

    lisätty e2e-testi bulbasaurin statseille

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,24 @@ describe("Pokedex", function() {
     cy.contains("ivysaur");
     cy.contains("Pokémon and Pokémon character names are trademarks of Nintendo.");
   });
+  it("Bulbasaur specs are rendered correctly", function() {
+    const expectedStats = [
+      ["speed", '45'],
+      ['special defense', '65'],
+      ['special attack', '65'],
+      ['defense', '49'],
+      ['attack', '49'],
+      ['hp', '45'],
+    ]
+    cy.visit("");
+    cy.contains("bulbasaur").click();
+    cy.get('[data-testid="stats"] tbody tr')
+      .should("have.length", expectedStats.length)
+      .each(($row, index) => {
+        cy.wrap($row).find("td").eq(0)
+          .should("have.text", expectedStats[index][0])
+        cy.wrap($row).find("td").eq(1)
+          .should("have.text", expectedStats[index][1])
+      })
+  });
 });
