21b246c7bb656663d4f60b31a843d1425270296e
rprinne <rp.rinne@hotmail.com>
2025-12-31 23:22:42 +0200

    korjattu cypressin linttaus

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -6,22 +6,22 @@ describe("Pokedex", function() {
   });
   it("Bulbasaur specs are rendered correctly", function() {
     const expectedStats = [
-      ["speed", '45'],
-      ['special defense', '65'],
-      ['special attack', '65'],
-      ['defense', '49'],
-      ['attack', '49'],
-      ['hp', '45'],
-    ]
+      ["speed", "45"],
+      ["special defense", "65"],
+      ["special attack", "65"],
+      ["defense", "49"],
+      ["attack", "49"],
+      ["hp", "45"],
+    ];
     cy.visit("");
     cy.contains("bulbasaur").click();
-    cy.get('[data-testid="stats"] tbody tr')
+    cy.get("[data-testid=\"stats\"] tbody tr")
       .should("have.length", expectedStats.length)
       .each(($row, index) => {
         cy.wrap($row).find("td").eq(0)
-          .should("have.text", expectedStats[index][0])
+          .should("have.text", expectedStats[index][0]);
         cy.wrap($row).find("td").eq(1)
-          .should("have.text", expectedStats[index][1])
-      })
+          .should("have.text", expectedStats[index][1]);
+      });
   });
 });
