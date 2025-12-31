7a78d3edb1cefe746e095799edba1d8a170d2e57
rprinne <rp.rinne@hotmail.com>
2025-12-31 15:29:42 +0200

    korjattu lintaus

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route, useMatch } from 'react-router-dom'
-import { useApi } from './useApi'
-import LoadingSpinner from './LoadingSpinner'
-import ErrorMessage from './ErrorMessage'
-import PokemonPage from './PokemonPage'
-import PokemonList from './PokemonList'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useMatch } from "react-router-dom"; //eslint-disable-line
+import { useApi } from "./useApi";
+import LoadingSpinner from "./LoadingSpinner";
+import ErrorMessage from "./ErrorMessage";
+import PokemonPage from "./PokemonPage";
+import PokemonList from "./PokemonList";
 
 const mapResults = (({ results }) => results.map(({ url, name }) => ({
   url,
   name,
   id: parseInt(url.match(/\/(\d+)\//)[1])
-})))
+})));
 
 const App = () => {
-  const match = useMatch('/pokemon/:name')
-  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
+  const match = useMatch("/pokemon/:name");
+  const { data: pokemonList, error, isLoading } = useApi("https://pokeapi.co/api/v2/pokemon/?limit=50", mapResults);
 
   if (isLoading) {
-    return <LoadingSpinner />
+    return <LoadingSpinner />;
   }
   if (error) {
-    return <ErrorMessage error={error} />
+    return <ErrorMessage error={error} />;
   }
 
-  let next = null
-  let previous = null
+  let next = null;
+  let previous = null;
 
   if (match && match.params) {
-    const pokemonId = pokemonList.find(({ name }) => name === match.params.name).id
-    previous = pokemonList.find(({ id }) => id === pokemonId - 1)
-    next = pokemonList.find(({ id }) => id === pokemonId + 1)
+    const pokemonId = pokemonList.find(({ name }) => name === match.params.name).id;
+    previous = pokemonList.find(({ id }) => id === pokemonId - 1);
+    next = pokemonList.find(({ id }) => id === pokemonId + 1);
   }
 
   return (
@@ -39,7 +39,7 @@ const App = () => {
         <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
       } />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
